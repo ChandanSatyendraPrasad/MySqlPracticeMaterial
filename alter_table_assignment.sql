4df354e32015c6cdb952dad243933b256123916f
ChandanSatyendraPrasad <chandansatyendraprasad@gmail.com>
2022-05-31 15:51:14 +0530

    Add files via upload

---
+++ alter_table_assignment.sql
@@ -0,0 +1,12 @@
+/*Use SQL to Alter Database Table
+• Add columns to Book table for ASIN, UPC, EAN.
+• ASIN - Amazon Standard Identification Number - 10 Alphanumeric characters
+• UPC - Universal Product Code - 12 Numeric Digits
+• EAN - International / European Article Number - 13 Numeric Digits
+• Hint: Use Decimal for columns with numeric digits*/
+use employees;
+ALTER TABLE book ADD COLUMN asin char(10) after isbn;
+
+ALTER TABLE book ADD COLUMN upc decimal(12, 0) after asin;
+
+ALTER TABLE book ADD COLUMN ean decimal(13, 0) after upc
